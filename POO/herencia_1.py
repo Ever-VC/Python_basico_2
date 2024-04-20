01fba7e1d6300a26e317b06f8b89a5264fd8f5da
Ever VC <ever.vasquezc27@gmail.com>
2024-04-20 10:33:39 -0600

    Verificar si un objeto es instancia de una clase X

--- POO/herencia_1.py
+++ POO/herencia_1.py
@@ -64,4 +64,54 @@ for cls1 in [Vehicle, LandVehicle, TrackedVehicle]:
     for cls2 in [Vehicle, LandVehicle, TrackedVehicle]:
         print(issubclass(cls1, cls2), end="\t")
     print()
-    
+
+'''
+Como ya sabes, un objeto es la encarnación de una clase. 
+Esto significa que el objeto es como un pastel horneado 
+usando una receta que se incluye dentro de la clase.
+
+Esto puede generar algunos problemas.
+
+Supongamos que tienes un pastel (por ejemplo, resultado de un 
+argumento pasado a tu función). Deseas saber que receta se ha 
+utilizado para prepararlo. ¿Por qué? Porque deseas saber que 
+esperar de él, por ejemplo, si contiene nueces o no, lo cual 
+es información crucial para ciertas personas.
+
+Del mismo modo, puede ser crucial si el objeto tiene (o no tiene) 
+ciertas características. En otras palabras, si es un objeto de 
+cierta clase o no.
+
+Tal hecho podría ser detectado por la función llamada isinstance():
+
+```isinstance(objectName, ClassName)```
+
+La función devuelve True si el objeto es una instancia de la clase, o 
+False de lo contrario.
+
+Ser una instancia de una clase significa que el objeto (el pastel) se 
+ha preparado utilizando una receta contenida en la clase o en una de 
+sus superclases.
+
+No lo olvides: si una subclase contiene al menos las mismas 
+características que cualquiera de sus superclases, significa que 
+los objetos de la subclase pueden hacer lo mismo que los objetos 
+derivados de la superclase, por lo tanto, es una instancia de su 
+clase de inicio y cualquiera de sus superclases.
+'''
+
+print("---------------------")
+my_vehicle = Vehicle()
+my_land_vehicle = LandVehicle()
+my_tracked_vehicle = TrackedVehicle()
+for obj in [my_vehicle, my_land_vehicle, my_tracked_vehicle]:
+    for cls in [Vehicle, LandVehicle, TrackedVehicle]:
+        print(isinstance(obj, cls), end="\t")
+    print()
+
+'''
+Hemos creado tres objetos, uno para cada una de las clases. 
+Luego, usando dos bucles anidados, verificamos todos los 
+pares posibles de clase de objeto para averiguar si los 
+objetos son instancias de las clases.
+'''
