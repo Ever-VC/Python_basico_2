ebca4f8ed72333a471eb7d89fdd9b03947503429
Ever VC <ever.vasquezc27@gmail.com>
2024-05-01 11:09:03 -0600

    Escribiendo lineas enteras en archivos txt

---
+++ Archivos/Procesando_archivos/escrbiendo_en_archivos_2.py
@@ -0,0 +1,18 @@
+'''
+Mira el ejemplo en el editor. Hemos modificado el código anterior para escribir líneas enteras en el archivo de texto.
+'''
+from os import strerror
+
+try:
+    file = open('./Archivos/Procesando_archivos/newtext.txt', 'wt')
+    for i in range(10):
+        file.write("línea #" + str(i+1) + "\n")
+    file.close()
+except IOError as e:
+    print("Se produjo un error de E/S: ", strerror(e.errno))
+    
+'''
+El contenido del archivo recién creado es el mismo.
+
+Nota: puedes usar el mismo método para escribir en el stream stderr, pero no intentes abrirlo, ya que siempre está abierto implícitamente.
+'''
